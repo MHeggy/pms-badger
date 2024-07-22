5d9a861d1e67ca67f825f9a195b3dfca23071aed
MHeggy <mhegedui@iu.edu>
2024-07-22 13:33:15 -0400

    updated calendar.php to displayEventTime: true

--- app/Views/PMS/calendar.php
+++ app/Views/PMS/calendar.php
@@ -79,7 +79,7 @@
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
-            showEventTime: 'true',
+            displayEventTime: true,
             headerToolbar: {
                 left: 'prev,next',
                 center: 'title',
