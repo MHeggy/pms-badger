02c68a9a90a941fd30526e37e54398b2ee9e2a56
MHeggy <mhegedui@iu.edu>
2024-07-31 10:40:13 -0400

    header.php slight pageTitle modification

--- app/Views/PMS/header.php
+++ app/Views/PMS/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $pageTitle ?></title>
+    <title><?= isset($pageTitle) ? $pageTitle : 'PMSBadger' ?></title>
     <!-- Script for bootstrap and jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
