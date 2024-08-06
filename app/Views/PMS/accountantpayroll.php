68131fadb26cd245e6c3a717d74c364cd556df50
MHeggy <mhegedui@iu.edu>
2024-08-06 12:28:58 -0400

    edited view

--- app/Views/PMS/accountantpayroll.php
+++ app/Views/PMS/accountantpayroll.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Payroll [Accountant]</title>
+    <title><?php $pageTitle = 'Payroll [Accountant]' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
