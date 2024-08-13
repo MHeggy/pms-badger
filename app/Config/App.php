8a800903d5b03231af9849b73eebc6435ebcbe23
MHeggy <mhegedui@iu.edu>
2024-08-13 10:17:21 -0400

    reverted a change to baseURL LOL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'pmsbadger.com/';
+    public string $baseURL = 'http://159.223.121.173/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
