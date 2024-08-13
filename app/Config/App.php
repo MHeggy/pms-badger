4a31bcf871be9e5dfdd03d9b431ccb62e6ec6f76
MHeggy <mhegedui@iu.edu>
2024-08-13 11:50:34 -0400

    changed baseURL variable

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://159.223.121.173/';
+    public string $baseURL = 'http://pmsbadger.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
