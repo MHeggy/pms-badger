e4588a917a745eb4d1867ea0c5623a2745ac7e47
MHeggy <mhegedui@iu.edu>
2024-08-20 11:49:56 -0400

    slight changes to accountantpayroll.php view

--- app/Views/PMS/accountantpayroll.php
+++ app/Views/PMS/accountantpayroll.php
@@ -15,12 +15,11 @@
             <label for="username" class="form-label">Filter by Username</label>
             <select name="username" id="username" class="form-select">
                 <option value="">All Users</option>
-                <?php foreach ($usernames as $user): ?>
-                    <option value="<?= esc($user->username) ?>" <?= set_select('username', $user->username) ?>>
-                        <?= esc($user->username) ?>
+                <?php foreach ($usernames as $username): ?>
+                    <option value="<?= esc($username) ?>" <?= set_select('username', $username) ?>>
+                        <?= esc($username) ?>
                     </option>
                 <?php endforeach; ?>
-
             </select>
         </div>
 
