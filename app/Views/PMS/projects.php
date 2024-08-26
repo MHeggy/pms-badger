3cc42296df7adf8d8095bed53368ab165a82b513
MHeggy <mhegedui@iu.edu>
2024-08-26 16:08:12 -0400

    added the main.js to the projects.php view file

--- app/Views/PMS/projects.php
+++ app/Views/PMS/projects.php
@@ -106,5 +106,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 </body>
 </html>
