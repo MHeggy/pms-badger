93275c10951c0c6938d031bfbd203833cf79b25d
MHeggy <mhegedui@iu.edu>
2024-08-26 15:15:36 -0400

    probably broke the mywork.php view file but changed the  to  in the foreach just to see something

--- app/Views/PMS/mywork.php
+++ app/Views/PMS/mywork.php
@@ -77,7 +77,7 @@
         </thead>
         <!-- Table body -->
         <tbody id="project_list">
-        <?php foreach ($projects as $project): ?>
+        <?php foreach ($assignedProjects as $project): ?>
             <tr data-project-id="<?= $project['projectID'] ?>">
                 <td>
                     <a href="<?= base_url('projects/details/' . $project['projectID']) ?>">
