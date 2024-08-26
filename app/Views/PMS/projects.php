b26aa1fc5149b105745a15e92e2bf053c456e3b8
MHeggy <mhegedui@iu.edu>
2024-08-26 16:12:29 -0400

    having to fix the styling so that the dropdown works from each page

--- app/Views/PMS/projects.php
+++ app/Views/PMS/projects.php
@@ -1,7 +1,7 @@
 <?= $pageTitle = "Projects"; ?>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
     <!-- Header content -->
     <div id="header">
         <header>
