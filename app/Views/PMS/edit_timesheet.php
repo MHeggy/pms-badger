fcd53f1207147a259f5f042b8da74a26b2281579
MHeggy <mhegedui@iu.edu>
2024-08-27 11:53:43 -0400

    slight change to the text of the button

--- app/Views/PMS/edit_timesheet.php
+++ app/Views/PMS/edit_timesheet.php
@@ -26,7 +26,7 @@
         <?= session()->get('info_message') ?>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-    <button onclick="goBack()" class="btn btn-primary btn-back">Back to Timesheets</button>
+    <button onclick="goBack()" class="btn btn-primary btn-back">Go Back</button>
     <form id="timesheet-form" action="/timesheets/update" method="post">
         <input type="hidden" name="id" value="<?= esc($timesheet['timesheetID']) ?>">
 
