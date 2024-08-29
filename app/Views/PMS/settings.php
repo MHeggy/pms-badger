a09c0820596348950f0a8e8fe901c6ce1bdd0d60
MHeggy <mhegedui@iu.edu>
2024-08-29 16:39:26 -0400

    removing more unnecessary script tags that are messing up the dropdown menu of the header a bit

--- app/Views/PMS/settings.php
+++ app/Views/PMS/settings.php
@@ -88,7 +88,6 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 </body>
 </html>
