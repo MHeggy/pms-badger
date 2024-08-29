d3d244ecfa2effaa23e8a138c6743db04aef6ac7
MHeggy <mhegedui@iu.edu>
2024-08-29 16:24:14 -0400

    projects.php updated

--- app/Views/PMS/projects.php
+++ app/Views/PMS/projects.php
@@ -1,6 +1,5 @@
 <?= $pageTitle = "Projects"; ?>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
     <!-- Header content -->
