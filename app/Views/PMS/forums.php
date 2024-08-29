937cea96327c1c60e9f1b40ede5b45c508297938
MHeggy <mhegedui@iu.edu>
2024-08-29 16:37:41 -0400

    more unnecessary scripts removed from forums.php

--- app/Views/PMS/forums.php
+++ app/Views/PMS/forums.php
@@ -69,7 +69,6 @@
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo base_url('/assets/js/messages.js') ?>"></script>
     <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
     <!-- Include TinyMCE library -->
