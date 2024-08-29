ff4ba8af4c8d3c95aebfff64e396a1863106371e
MHeggy <mhegedui@iu.edu>
2024-08-29 16:22:32 -0400

    trying to figure out why dropdown doesn't work

--- app/Views/PMS/projects.php
+++ app/Views/PMS/projects.php
@@ -1,6 +1,7 @@
 <?= $pageTitle = "Projects"; ?>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
     <!-- Header content -->
     <div id="header">
@@ -105,7 +106,6 @@
         </table>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 </body>
 </html>
