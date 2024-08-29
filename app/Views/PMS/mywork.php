28e27f2e70006df1afe5befde4357262c90eb533
MHeggy <mhegedui@iu.edu>
2024-08-29 10:31:20 -0400

    changes in css

--- app/Views/PMS/mywork.php
+++ app/Views/PMS/mywork.php
@@ -1,5 +1,5 @@
 <?= $pageTitle = "My Work"; ?>
-<link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/mywork.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Header content -->
