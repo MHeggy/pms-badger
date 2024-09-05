8264da8ddfd55b5cc805776dfd22385b19c4fd03
MHeggy <mhegedui@iu.edu>
2024-09-05 14:05:25 -0400

    added styling to forgot_password.php file

--- app/Views/PMS/forgot_password.php
+++ app/Views/PMS/forgot_password.php
@@ -8,6 +8,11 @@
 <!-- Style sheet -->
 <link rel="stylesheet" href="<?= base_url('/assets/css/login.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<style>
+    .container {
+        margin-top: 80px;
+    }
+</style>
 <!-- Body content -->
 <div class="container">
     <h2>Forgot Password</h2>
