dff3c0bfba03480c53e7188613d108d563be68b3
MHeggy <mhegedui@iu.edu>
2024-09-05 15:22:24 -0400

    edit function updated again

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -345,9 +345,9 @@ class ProjectsController extends BaseController {
     public function edit($projectID) {
         $project = $this->$projectModel->findProjectDetails($projectID);
     
-        if (!$project) {
-            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound('Project not found');
-        }
+        // if (!$project) {
+        //     throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound('Project not found');
+        // }
     
         $data = [
             'project' => $project
