3c1850996b8d899576d68907c8c18daf2282a6ae
MHeggy <mhegedui@iu.edu>
2024-09-05 15:30:28 -0400

    added edit_project.css file and linked it to the edit_project.php view file

--- app/Views/PMS/edit_project.php
+++ app/Views/PMS/edit_project.php
@@ -1,7 +1,8 @@
 <?= $pageTitle = "Edit Project"; ?>
-<link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/edit_project.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
 <!-- Header content -->
 <div id="header">
     <header>
