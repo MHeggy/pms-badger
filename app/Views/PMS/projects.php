7f148cb77d3b9dddf3a0648945d15bbfb84d054b
MHeggy <mhegedui@iu.edu>
2024-09-05 15:17:59 -0400

    fixed the other issue, getting rid of var_dump() from the projects view

--- app/Views/PMS/projects.php
+++ app/Views/PMS/projects.php
@@ -1,4 +1,3 @@
-<?php var_dump($user1); ?>
 <?= $pageTitle = "Projects"; ?>
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
