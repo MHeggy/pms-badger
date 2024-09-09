0a694c25caff394b97112d2ad2c11b453b055195
MHeggy <mhegedui@iu.edu>
2024-09-09 11:31:00 -0400

    changing the variable we access from the  variable

--- app/Views/PMS/updateprofile.php
+++ app/Views/PMS/updateprofile.php
@@ -42,11 +42,11 @@
         </div>
         <div class="mb-3">
             <label for="firstName" class="form-label">First Name:</label>
-            <input type="text" class="form-control" id="firstName" name="first_name" value="<?= $user->first_name ?>">
+            <input type="text" class="form-control" id="firstName" name="first_name" value="<?= $user->firstName ?>">
         </div>
         <div class="mb-3">
             <label for="lastName" class="form-label">Last Name:</label>
-            <input type="text" class="form-control" id="lastName" name="last_name" value="<?= $user->last_name ?>">
+            <input type="text" class="form-control" id="lastName" name="last_name" value="<?= $user->lastName ?>">
         </div>
         <div class="mb-3">
             <label for="username" class="form-label">Username:</label>
