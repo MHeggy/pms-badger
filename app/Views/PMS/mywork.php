317eef4325850b5bd8bc5cdbae0129657bd266a9
MHeggy <mhegedui@iu.edu>
2024-09-09 11:09:09 -0400

    updated styling for mywork once again!

--- app/Views/PMS/mywork.php
+++ app/Views/PMS/mywork.php
@@ -2,7 +2,7 @@
 <?= $pageTitle = "My Work"; ?>
 
 <!-- Include CSS for styling -->
-<link rel="stylesheet" href="<?php echo base_url('/assets/css/projects.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/mywork.css') ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
 
