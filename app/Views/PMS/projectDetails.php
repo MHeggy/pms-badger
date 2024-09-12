a47cfef0de0f696407630c9583454131cc603f3f
MHeggy <mhegedui@iu.edu>
2024-09-12 10:44:29 -0400

    changes to projectDetails.php view file

--- app/Views/PMS/projectDetails.php
+++ app/Views/PMS/projectDetails.php
@@ -209,7 +209,9 @@
     </div>
 </div>
 <?php endforeach; ?>
-<script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url('/assets/js/main.js')?>"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="<?php echo base_url('/assets/js/projects.js') ?>"></script>
 </body>
 </html>
