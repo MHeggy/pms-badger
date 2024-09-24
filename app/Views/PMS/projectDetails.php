a7d57fc9a2b4efe012111ee5facbfd5298130840
MHeggy <mhegedui@iu.edu>
2024-09-24 12:27:19 -0400

    changes again

--- app/Views/PMS/projectDetails.php
+++ app/Views/PMS/projectDetails.php
@@ -221,3 +221,15 @@
 <!-- JavaScript files -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+<script>
+    // Example for checking modal events
+    var myModal = document.getElementById('addUpdateModal')
+        myModal.addEventListener('show.bs.modal', function (event) {
+        console.log('Modal is opening');
+    })
+
+    myModal.addEventListener('hidden.bs.modal', function (event) {
+        console.log('Modal is closed');
+    })
+
+</script>
