d67a14f2d6dbf0a412a94bc6bfe362730463bdc8
MHeggy <mhegedui@iu.edu>
2024-09-25 11:13:59 -0400

    slight change to the header file again

--- app/Views/PMS/header.php
+++ app/Views/PMS/header.php
@@ -56,7 +56,7 @@
                         <a class="nav-link <?= $pageTitle == 'Projects' ? 'active' : '' ?>" href="<?php echo base_url('/projects') ?>">Projects</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?= $pageTitle == 'Forums' ? 'active' : '' ?>" href="<?php echo base_url('/forums') ?>">Projects</a>
+                        <a class="nav-link <?= $pageTitle == 'Forums' ? 'active' : '' ?>" href="<?php echo base_url('/forums') ?>">Forums</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?= $pageTitle == 'My Work' ? 'active' : '' ?>" href="<?php echo base_url('/my_work') ?>">My Work</a>
