9547f5ad2bb10464dec958bde7d7f3999321d756
MHeggy <mhegedui@iu.edu>
2024-09-26 10:43:59 -0400

    changing up the html elements once again

--- app/Views/PMS/calendar.php
+++ app/Views/PMS/calendar.php
@@ -1,15 +1,18 @@
 <?php $pageTitle = "Calendar" ?>
-    <!-- Link to Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Script for FullCalendar -->
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
-    <!-- Script for jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="<?php echo base_url('/assets/css/calendar.css')?>">
+
 <header>
     <?php include 'header.php' ?>
 </header>
+
+<!-- Link to Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Script for FullCalendar -->
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
+<!-- Script for jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<!-- Custom CSS -->
+<link rel="stylesheet" href="<?php echo base_url('/assets/css/calendar.css')?>">
+
 <br><br><br>
 <!-- Calendar Section -->
 <div id="calendar"></div>
