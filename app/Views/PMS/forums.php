9620db0fa64365fcdadeccc5d79e97c6645f5d6a
MHeggy <mhegedui@iu.edu>
2024-09-26 10:19:11 -0400

    changes once again

--- app/Views/PMS/forums.php
+++ app/Views/PMS/forums.php
@@ -64,9 +64,6 @@
                 <div class="card mb-2">
                     <div class="card-body p-2 p-sm-3">
                         <div class="media forum-item">
-                            <a href="#" data-toggle="collapse" data-target=".forum-content">
-                                <img src="<?= base_url('/assets/img/avatars/' . $post['user_avatar']) ?>" class="mr-3 rounded-circle" width="50" alt="User" />
-                            </a>
                             <div class="media-body">
                                 <h6><a href="<?= base_url('/forums/post/' . $post['id']) ?>" class="text-body"><?= $post['title'] ?></a></h6>
                                 <p class="text-secondary"><?= $post['excerpt'] ?></p>
