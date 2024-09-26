6f5f7354bb9a444471271c3b74a4d186f207bc35
MHeggy <mhegedui@iu.edu>
2024-09-26 10:30:22 -0400

    just trying something

--- app/Views/PMS/user_timesheets.php
+++ app/Views/PMS/user_timesheets.php
@@ -1,7 +1,6 @@
 <?php esc($pageTitle = $user['username'] . ' Timesheets') ?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 <style>
     body {
         font-family: 'Roboto', sans-serif;
