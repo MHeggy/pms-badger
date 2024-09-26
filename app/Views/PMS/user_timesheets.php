471fd66c6566567174c9efcb581b5280bd25b610
MHeggy <mhegedui@iu.edu>
2024-09-26 10:36:06 -0400

    once again trying to fix the modal issues ive been running into

--- app/Views/PMS/user_timesheets.php
+++ app/Views/PMS/user_timesheets.php
@@ -97,7 +97,7 @@
         window.history.back();
     }
 </script>
-<script src="<?php echo base_url('/assets/js/main.js')?>"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
