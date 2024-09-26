d98b58fa80645b17ad41baf07262f930c8399992
MHeggy <mhegedui@iu.edu>
2024-09-26 10:28:19 -0400

    changes again

--- app/Views/PMS/forums.php
+++ app/Views/PMS/forums.php
@@ -79,6 +79,5 @@
                 </div>
             <?php endforeach; ?>
         </div>
-
     </div>
 </main>
--- app/Views/PMS/user_timesheets.php
+++ app/Views/PMS/user_timesheets.php
@@ -98,5 +98,6 @@
     }
 </script>
 <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
