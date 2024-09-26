d78c9437093ba9569b9abf72e452596994ef4589
MHeggy <mhegedui@iu.edu>
2024-09-26 10:32:47 -0400

    trying something

--- app/Views/PMS/user_timesheets.php
+++ app/Views/PMS/user_timesheets.php
@@ -96,6 +96,8 @@
     function goBack() {
         window.history.back();
     }
+
+    $('#confirmDeleteModal').prependTo('body');
 </script>
 <script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
