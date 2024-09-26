fe3ae99ed0ef1589b29dd7dccfa17eacf174c75f
MHeggy <mhegedui@iu.edu>
2024-09-26 10:07:24 -0400

    removing the messages.css linked file to see what happens

--- app/Views/PMS/forumdetails.php
+++ app/Views/PMS/forumdetails.php
@@ -1,5 +1,5 @@
 <?php $pageTitle = $post['title']; ?>
-<link rel="stylesheet" href="<?php echo base_url('/assets/css/messages.css') ?>">
+
 <!-- Link to Bootstrap CSS and FontAwesome -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
