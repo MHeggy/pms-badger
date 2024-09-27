24493303d725f29157d764472d1847addfecbc96
MHeggy <mhegedui@iu.edu>
2024-09-27 14:32:51 -0400

    changing styling of the project-card

--- app/Views/PMS/mywork.php
+++ app/Views/PMS/mywork.php
@@ -29,11 +29,12 @@
         background-color: #ffc107; /* Yellow for 'Cancelled' or 'Postponed' */
         color: #212529; /* Dark text */
     }
+    
     .project-card {
-        border: 1px solid #e0e0e0;
-        border-radius: 10px;
-        background-color: #f9f9f9;
+        background-color: white;
+        border-radius: 8px;
         padding: 20px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
     }
 </style>
 
